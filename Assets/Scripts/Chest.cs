6b195f379593b33a64b402be293e21fcffc9ff85
Albert Han <alberthan@Alberts-MacBook-Pro.local>
2019-09-12 00:14:21 -0700

    first submit

---
+++ Assets/Scripts/Chest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    #region gameObject_variables
+    [SerializeField]
+    [Tooltip("HealthPack")]
+    private GameObject healthPack;
+    #endregion
+
+    #region heper_function
+    IEnumerator DeleteChest()
+    {
+        yield return new WaitForSeconds(0.3f);
+        Instantiate(healthPack, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+
+    public void Interact()
+    {
+        StartCoroutine(DeleteChest());
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/LineofSight.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineofSight : MonoBehaviour
+{
+    //called when something enters the trigger collider
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        //check if tag is player
+        if (coll.CompareTag("Player"))
+        {
+            GetComponentInParent<Enemy>().player = coll.transform;
+            Debug.Log("See Player run at player");
+        }
+    }
+}
